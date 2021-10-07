ce008ff5386774a4f2ec73b2c24a19ff639b83e2
Tien Tran <tqtteams1st@gmail.com>
2021-10-07 14:54:00 +0700

    variable Java

---
+++ variableJava.java
@@ -0,0 +1,18 @@
+package com.tientranjav;
+
+public class variableJava {
+    public static void main(String[] args) {
+        int a = 10;
+        float f = 15.6f;
+        double d = 15.6;
+        boolean b = true;
+        char c = 'a';
+        String s = "Ha noi";
+        System.out.println("i = " + a);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}
